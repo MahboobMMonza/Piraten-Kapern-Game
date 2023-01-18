6771cf674853e4030224f248dc9b70ef248bb9b2
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-01-18 12:45:03 -0500

    Started player class

---
+++ src/main/java/pk/Player.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Player {
+
+    // public Strategy strats;
+    public Random rand;
+    public int score;
+    public int wins;
+    public int[] dice;
+
+    public Player() {
+        score = 0;
+        wins = 0;
+        rand = new Random();
+        dice = new int[1];
+    }
+
+    public static void roll() {
+        for (int i = 0; i < dice.length; i++) {
+            dice[i] = rand.nextInt(0,6);
+        }
+    }
+}
