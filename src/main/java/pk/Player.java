b24ff38b8f4c0142489f0dbcfd3d5fbd8d37e0f8
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-01-21 22:08:27 -0500

    F01 Player features and updating Dice to be less redundant

--- src/main/java/pk/Player.java
+++ src/main/java/pk/Player.java
@@ -5,23 +5,46 @@ import java.util.*;
 public class Player {
 
     // public Strategy strats;
-    public Random rand;
+    private Random rand;
     public int score;
     public int wins;
-    public int[] dice;
+    public Dice dice;
+    public Faces[] diceFaces;
+    public static final int NUM_DICE = 8;
 
     public Player() {
         score = 0;
         wins = 0;
         rand = new Random();
-        dice = new int[1];
+        dice = new Dice();
+        diceFaces = new Faces[NUM_DICE];
     }
 
     public void roll() {
-        for (int i = 0; i < dice.length; i++) {
-            dice[i] = rand.nextInt(0,6);
+        for (int i = 0; i < NUM_DICE; i++) {
+            diceFaces[i] = dice.roll(rand);
         }
     }
 
-    public void playTurn() {}
+    public void resetScore() {
+        this.score = 0;
+    }
+
+    public void updateScore() {
+        for (Faces face : diceFaces) {
+            if (face == Faces.GOLD || face == Faces.DIAMOND) {
+                this.score += 100;
+            }
+        }
+    }
+
+    public void playTurn() {
+        // Turn playing logic - for now: roll all 8 dice once, then count score
+        roll();
+        updateScore();
+    }
+
+    public void winGame() {
+        this.wins++;
+    }
 }
