e192a571b225b1e673223f87cbc9bc8293f593bb
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-01-23 06:37:51 -0500

    Flushing debug output

--- src/main/java/pk/DebugLogger.java
+++ src/main/java/pk/DebugLogger.java
@@ -6,10 +6,10 @@ public class DebugLogger {
     private static Formatter formatter = new Formatter();
 
     public static void log(String logArgs) {
-        System.out.printf(" (DEBUG) %s\n", logArgs);
+        System.out.printf(" (DEBUG) %s%n", logArgs);
     }
 
     public static void logFormat(String format, Object... args) {
-        System.out.printf(" (DEBUG) %s\n", (formatter.format(format, args)).toString());
+        System.out.printf(" (DEBUG) %s%n", (formatter.format(format, args)).toString());
     }
 }
