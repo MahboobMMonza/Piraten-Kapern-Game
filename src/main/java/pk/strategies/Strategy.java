3d004e475fd4469d7d3b6a0b5de69e1db90ab30c
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-01-25 01:48:11 -0500

    Added logging to Strategy class

--- src/main/java/pk/strategies/Strategy.java
+++ src/main/java/pk/strategies/Strategy.java
@@ -1,6 +1,8 @@
 package pk.strategies;
 
 import java.util.Random;
+
+import pk.DebugLogger;
 import pk.Faces;
 import pk.GameManager;
 
@@ -19,26 +21,30 @@ public class Strategy {
 
     public StrategyMove strategize(Faces[] diceFaces) {
         move.resetRollList();
+        move.endTurn = false;
         int skullCount = 0, numDiceRoll, diceIndex;
         for (Faces face : diceFaces) {
             if (face == Faces.SKULL) {
                 skullCount++;
             }
         }
+        DebugLogger.logFormat("There are %d skulls currently rolled", skullCount);
+        // If 3 skulls (or more), then end the turn
         if (skullCount >= GameManager.DISQUALIFIED_SKULL_COUNT) {
             move.endTurn = true;
             return move;
         }
-        // Roll a random number of dice between 2 and max dice rolled, don't roll skulls
+        // Roll a random number of dice between 2 and max dice rolled; don't roll skulls (skullCount <= 2)
         numDiceRoll = rand.nextInt(NUM_DICE - skullCount - MIN_NUM_DICE_ROLLED + 1) + MIN_NUM_DICE_ROLLED;
+        DebugLogger.logFormat("Rolling %d dice", numDiceRoll);
         for (int i = 0; i < numDiceRoll; i++) {
             diceIndex = rand.nextInt(NUM_DICE);
             while (move.isRolled(diceIndex) || diceFaces[diceIndex] == Faces.SKULL) {
                 diceIndex = rand.nextInt(NUM_DICE);
             }
+            DebugLogger.logFormat("Rolling the dice at index %d", diceIndex);
             move.makeRoll(diceIndex);
         }
-        move.endTurn = false;
         return move;
     }
 }
