632311f8d4759bc38044df0f968457b0b0fe2be2
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-01-24 23:01:09 -0500

    Assigning Random to Dice instead of player - Dice

--- src/main/java/pk/Dice.java
+++ src/main/java/pk/Dice.java
@@ -1,11 +1,17 @@
 package pk;
+
 import java.util.Random;
 
 public class Dice {
 
     private static final Faces[] faceValues = Faces.values();
+    public Random bag;
 
-    public Faces roll(Random bag) {
+    public Dice() {
+        bag = new Random();
+    }
+
+    public Faces roll() {
         return faceValues[bag.nextInt(faceValues.length)];
     }
 
