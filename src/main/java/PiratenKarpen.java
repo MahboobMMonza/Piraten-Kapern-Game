c96a8ae54b6014034477c4b6ac56814cb9a3a45c
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-01-27 05:41:26 -0500

    Updated PiratenKarpen main method entry point for game

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -3,10 +3,16 @@ import pk.*;
 public class PiratenKarpen {
 
     public static void main(String[] args) {
-        GameManager manager = new GameManager();
-        double[] percentages = manager.getPercentages();
-        for (int i = 0; i < percentages.length; i++) {
-            System.out.println(String.format("Player %d win percentage: %f%%", i + 1, percentages[i]));
+        try {
+            GameManager manager = new GameManager(args, 3);
+            double[] percentages = manager.getPercentages();
+            for (int i = 0; i < percentages.length; i++) {
+                System.out.println(String.format("Player %d win percentage: %f%%", i + 1, percentages[i]));
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            System.out.println("GAME FAILED. TERMINATING.");
+            System.exit(-1);
         }
     }
 
