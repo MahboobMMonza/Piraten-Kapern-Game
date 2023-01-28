1de02e1edaa4de5d89f701518a84b35306f24b10
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-01-28 17:25:04 -0500

    Updated CardDeck with response to Card responsibilities and constructors, and restructured CARD_INFO storage

--- src/main/java/pk/CardDeck.java
+++ src/main/java/pk/CardDeck.java
@@ -1,10 +1,7 @@
 package pk;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 /**
  * CardDeck
@@ -18,27 +15,25 @@ public class CardDeck {
 
     private static final int NUM_CARDS_PER_SEA_BATTLE = 2;
     private static final int NUM_NOP_CARDS = 29;
-    private static final int NOP_CARD_VALUE = 0;
-    private static final int NOP_CARD_BONUS = 0;
-    private static final Map<Integer, Integer> SEA_BATTLE_INFO;
+    private static final int[][] SEA_BATTLE_INFO;
 
     static {
-        SEA_BATTLE_INFO = new TreeMap<>();
-        SEA_BATTLE_INFO.put(2, 300);
-        SEA_BATTLE_INFO.put(3, 500);
-        SEA_BATTLE_INFO.put(4, 1000);
+        SEA_BATTLE_INFO = new int[3][2];
+        SEA_BATTLE_INFO[0] = new int[] { 2, 300 };
+        SEA_BATTLE_INFO[1] = new int[] { 3, 500 };
+        SEA_BATTLE_INFO[2] = new int[] { 4, 1000 };
     }
 
     public CardDeck() {
         deck = new ArrayList<Card>();
         // 3 types * 2 cards per type = 6 cards
-        for (Map.Entry<Integer, Integer> entry : SEA_BATTLE_INFO.entrySet()) {
+        for (int[] info : SEA_BATTLE_INFO) {
             for (int i = 0; i < NUM_CARDS_PER_SEA_BATTLE; i++) {
-                deck.add(new Card(CardTypes.SEA_BATTLE, entry.getKey().intValue(), entry.getValue().intValue()));
+                deck.add(new Card(CardTypes.SEA_BATTLE, info[0], info[1]));
             }
         }
         for (int i = 0; i < NUM_NOP_CARDS; i++) {
-            deck.add(new Card(CardTypes.NOP, NOP_CARD_VALUE, NOP_CARD_BONUS));
+            deck.add(new Card(CardTypes.NOP));
         }
         currentIndex = 0;
         CARD_DECK_SIZE = deck.size();
