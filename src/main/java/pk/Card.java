e6c837a2cda5af8388787a0f78064b8480e4c049
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-01-28 17:24:01 -0500

    Updated Card with respobsibility of holding NOP defaults and second constructor for NOP cards

--- src/main/java/pk/Card.java
+++ src/main/java/pk/Card.java
@@ -8,6 +8,8 @@ public class Card {
     CardTypes cardType;
 
     public final int VALUE, BONUS_POINTS;
+    public static final int NOP_CARD_VALUE = 0;
+    public static final int NOP_CARD_BONUS = 0;
 
     protected Card(CardTypes type, int value, int bonus) {
         cardType = type;
@@ -20,5 +22,18 @@ public class Card {
                 VALUE = BONUS_POINTS = 0;
                 break;
         }
+    }
+
+    protected Card(CardTypes type) throws IllegalArgumentException {
+        cardType = type;
+        switch (cardType) {
+            case SEA_BATTLE:
+                throw new IllegalArgumentException("ERROR: This constructor cannot be used for Sea Battle cards.");
+            default:
+                VALUE = NOP_CARD_VALUE;
+                BONUS_POINTS = NOP_CARD_BONUS;
+                break;
+        }
+
     }
 }
